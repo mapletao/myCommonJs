5f72a4bdfcfba3029638bce904972fabc52e2bf6
zhoutao <zhoutao@yingroup.com>
2018-02-06 15:06:54 +0800

    code merge

--- ajax/ajax.js
+++ ajax/ajax.js
@@ -22,9 +22,10 @@
         }
         this.setOpt(option);
         this.xmlhttp = this.create();
-        this[this.setting.type.toLowerCase()]();
+        this.request();
     };
     CreatXml.prototype = {
+        construct: CreatXml,
         setOpt: function(option) {
             this.setting = {
                 async: true,
@@ -54,10 +55,20 @@
         readystate: function() {
             var self = this;
             self.xmlhttp.onreadystatechange = function() {
-                if (this.readyState == 4 && this.status == 200) {
-                    clearTimeout(self.setting.setTimes);
-                    self.setting.complete();
-                    self.setting.success(this.responseText);
+                if (this.readyState == 4) {
+                    if (this.status == 200) {
+                        clearTimeout(self.setting.setTimes);
+                        self.setting.complete();
+                        var data = this.responseText;
+                        if (data && JSON.parse(data)) {
+                            data = JSON.parse(data);
+                        }
+                        self.setting.success(data);
+                    } else {
+                        clearTimeout(self.setting.setTimes);
+                        self.setting.complete();
+                        self.setting.error(this.responseText);
+                    }
                 }
             }
         },
@@ -65,44 +76,46 @@
             var self = this;
             self.setting.setTimes = setTimeout(function() {
                 self.setting.complete();
-                self.setting.error();
+                self.setting.error('time out');
                 self.xmlhttp.abort();
             }, self.setting.timeout);
         },
         para: function(data) {
-            var datastr = "";
-            if (data && Object.prototype.toString.call(data) === "[object Object]") {
-                for (var i in data) {
-                    for (var i = 0; i < length; i++) {
-                        datastr += i + "=" + data[i] + "&";
-                    }
-                }
-            } else if (data && typeof data === "string") {
-                datastr = data;
+            // var datastr = [];
+            // if (data && Object.prototype.toString.call(data) === "[object Object]") {
+            //     for (var i in data) {
+            //         datastr.push(i + '=' + data[i]);
+            //     }
+            //     datastr = datastr.join('&')
+            // } else {
+            //     datastr = data;
+            // }
+            // return JSON.stringify(data)
+            var datastr = JSON.stringify(data)
+            return datastr ? datastr : data
+        },
+        request: function() {
+            this.beforeSendHandle();
+            var obj = this.getSendInfo();
+            var type = this.setting.type.toLowerCase();
+            this.xmlhttp.open(type, obj.newurl, obj.async);
+            this.xmlhttp.setRequestHeader('Content-Type', 'text/plain;charset=UTF-8');
+            this.xmlhttp.send(obj.datastr);
+        },
+        beforeSendHandle: function() {
+            this.setting.beforeSend();
+            this.setTime();
+            this.readystate();
+        },
+        getSendInfo: function() {
+            var newurl = this.setting.url;
+            var async = this.setting.async;
+            var datastr = this.para(this.setting.data);
+            return {
+                newurl: newurl,
+                async: async,
+                datastr: datastr
             }
-            return datastr;
-        },
-        get: function() {
-            this.setting.beforeSend();
-            this.setTime();
-            this.readystate();
-            var newurl = this.setting.url;
-            var datastr = this.para(this.setting.data);
-            var async = this.setting.async;
-            newurl = this.setting.url + "?" + datastr;
-            this.xmlhttp.open("get", newurl, async);
-            this.xmlhttp.send(null);
-        },
-        post: function() {
-            this.setting.beforeSend();
-            this.setTime();
-            this.readystate();
-            var newurl = this.setting.url;
-            var async = this.setting.async;
-            var datastr = this.para(this.setting.data);
-            this.xmlhttp.open("post", newurl, async);
-            this.xmlhttp.setRequestHeader("content-type", "x-www-form-urlencoded");
-            this.xmlhttp.send(datastr);
         }
     };
     return CreatXml
