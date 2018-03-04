6bba914bd2c370eaf68e2ceedc53d1218bf1145a
zhoutao <zhoutao@yingroup.com>
2018-03-04 12:46:06 +0800

    h5 离线存储

--- ajax/ajax.js
+++ ajax/ajax.js
@@ -1,121 +1,121 @@
 ;
 (function(win, factory) {
-    typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
-        typeof define === 'function' && define.amd ? define(factory) :
-        (win['ztAjax'] = factory());
+  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+    typeof define === 'function' && define.amd ? define(factory) :
+    (win['ztAjax'] = factory());
 })(window, function() {
-    // options 
-    // url 地址
-    // type 请求方式
-    // async false 同步，true 异步 默认异步
-    // timeout 设置请求时间
-    // data 发送数据
-    // complete 
-    // beforeSend
-    // 创建对象
-    const CreatXml = function(option) {
-        if (!(this instanceof CreatXml)) {
-            return new CreatXml(option)
+  // options 
+  // url 地址
+  // type 请求方式
+  // async false 同步，true 异步 默认异步
+  // timeout 设置请求时间
+  // data 发送数据
+  // complete 
+  // beforeSend
+  // 创建对象
+  const CreatXml = function(option) {
+    if (!(this instanceof CreatXml)) {
+      return new CreatXml(option)
+    }
+    if (!option.url || !option.type) {
+      return false;
+    }
+    this.setOpt(option);
+    this.xmlhttp = this.create();
+    this.request();
+  };
+  CreatXml.prototype = {
+    constructor: CreatXml,
+    setOpt: function(option) {
+      this.setting = {
+        async: true,
+        timeout: 15000,
+        data: '',
+        success: function() {},
+        error: function() {},
+        complete: function() {},
+        beforeSend: function() {}
+      };
+      this.extend(this.setting, option);
+    },
+    extend: function(obj, obj2) {
+      for (var i in obj2) {
+        obj[i] = obj2[i];
+      }
+    },
+    create: function() {
+      var xmlhttp = "";
+      if (window.XMLHttpRequest) {
+        xmlhttp = new XMLHttpRequest();
+      } else {
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+      }
+      return xmlhttp;
+    },
+    readystate: function() {
+      var self = this;
+      self.xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+          if (this.status == 200) {
+            clearTimeout(self.setting.setTimes);
+            self.setting.complete();
+            var data = this.responseText;
+            if (data && JSON.parse(data)) {
+              data = JSON.parse(data);
+            }
+            self.setting.success(data);
+          } else {
+            clearTimeout(self.setting.setTimes);
+            self.setting.complete();
+            self.setting.error(this.responseText);
+          }
         }
-        if (!option.url || !option.type) {
-            return false;
-        }
-        this.setOpt(option);
-        this.xmlhttp = this.create();
-        this.request();
-    };
-    CreatXml.prototype = {
-        constructor: CreatXml,
-        setOpt: function(option) {
-            this.setting = {
-                async: true,
-                timeout: 15000,
-                data: '',
-                success: function() {},
-                error: function() {},
-                complete: function() {},
-                beforeSend: function() {}
-            };
-            this.extend(this.setting, option);
-        },
-        extend: function(obj, obj2) {
-            for (var i in obj2) {
-                obj[i] = obj2[i];
-            }
-        },
-        create: function() {
-            var xmlhttp = "";
-            if (window.XMLHttpRequest) {
-                xmlhttp = new XMLHttpRequest();
-            } else {
-                xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-            }
-            return xmlhttp;
-        },
-        readystate: function() {
-            var self = this;
-            self.xmlhttp.onreadystatechange = function() {
-                if (this.readyState == 4) {
-                    if (this.status == 200) {
-                        clearTimeout(self.setting.setTimes);
-                        self.setting.complete();
-                        var data = this.responseText;
-                        if (data && JSON.parse(data)) {
-                            data = JSON.parse(data);
-                        }
-                        self.setting.success(data);
-                    } else {
-                        clearTimeout(self.setting.setTimes);
-                        self.setting.complete();
-                        self.setting.error(this.responseText);
-                    }
-                }
-            }
-        },
-        setTime: function() {
-            var self = this;
-            self.setting.setTimes = setTimeout(function() {
-                self.setting.complete();
-                self.setting.error('time out');
-                self.xmlhttp.abort();
-            }, self.setting.timeout);
-        },
-        para: function(data) {
-            // var datastr = [];
-            // if (data && Object.prototype.toString.call(data) === "[object Object]") {
-            //     for (var i in data) {
-            //         datastr.push(i + '=' + data[i]);
-            //     }
-            //     datastr = datastr.join('&')
-            // } else {
-            //     datastr = data;
-            // }
-            // return JSON.stringify(data)
-            var datastr = JSON.stringify(data)
-            return datastr ? datastr : data
-        },
-        request: function() {
-            this.beforeSendHandle();
-            var obj = this.getSendInfo();
-            var type = this.setting.type.toLowerCase();
-            this.xmlhttp.open(type, obj.newurl, obj.async);
-            this.xmlhttp.send(obj.datastr);
-        },
-        beforeSendHandle: function() {
-            this.setting.beforeSend();
-            this.setTime();
-            this.readystate();
-        },
-        getSendInfo: function() {
-            var newurl = this.setting.url;
-            var async = this.setting.async;
-            var datastr = this.para(this.setting.data);
-            return {
-                newurl: newurl,
-                async: async,
-                datastr: datastr
-            }
-        }
-    };
-    return CreatXml
+      }
+    },
+    setTime: function() {
+      var self = this;
+      self.setting.setTimes = setTimeout(function() {
+        self.setting.complete();
+        self.setting.error('time out');
+        self.xmlhttp.abort();
+      }, self.setting.timeout);
+    },
+    para: function(data) {
+      // var datastr = [];
+      // if (data && Object.prototype.toString.call(data) === "[object Object]") {
+      //     for (var i in data) {
+      //         datastr.push(i + '=' + data[i]);
+      //     }
+      //     datastr = datastr.join('&')
+      // } else {
+      //     datastr = data;
+      // }
+      // return JSON.stringify(data)
+      var datastr = JSON.stringify(data)
+      return datastr ? datastr : data
+    },
+    request: function() {
+      this.beforeSendHandle();
+      var obj = this.getSendInfo();
+      var type = this.setting.type.toLowerCase();
+      this.xmlhttp.open(type, obj.newurl, obj.async);
+      this.xmlhttp.send(obj.datastr);
+    },
+    beforeSendHandle: function() {
+      this.setting.beforeSend();
+      this.setTime();
+      this.readystate();
+    },
+    getSendInfo: function() {
+      var newurl = this.setting.url;
+      var async = this.setting.async;
+      var datastr = this.para(this.setting.data);
+      return {
+        newurl: newurl,
+        async: async,
+        datastr: datastr
+      }
+    }
+  };
+  return CreatXml
 });
--- ajax/service.js
+++ ajax/service.js
@@ -10,18 +10,18 @@ app.use(urlencodedParser)
 
 
 function setHeader(res) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
-    res.header("X-Powered-By", ' 3.2.1')
-    res.header("Content-Type", "application/json;charset=utf-8");
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+  res.header("X-Powered-By", ' 3.2.1')
+  res.header("Content-Type", "application/json;charset=utf-8");
 }
 var methods = ['get', 'options', 'post', 'put', 'delete'];
 methods.forEach(function(item) {
-    app[item]('*', function(req, res, next) {
-        setHeader(res);
-        next();
-    });
+  app[item]('*', function(req, res, next) {
+    setHeader(res);
+    next();
+  });
 });
 // app.get('*', function(req, res, next) {
 //     setHeader(res);
@@ -45,58 +45,58 @@ methods.forEach(function(item) {
 // })
 
 app.get('/test-list', function(req, res) {
-    fs.readFile('test.json', function(err, data) {
-        if (err) return res.send(err);
-        var responsText = data
-        res.send(responsText)
-    })
+  fs.readFile('test.json', function(err, data) {
+    if (err) return res.send(err);
+    var responsText = data
+    res.send(responsText)
+  })
 })
 app.delete('/test-list/:id', function(req, res) {
-    res.send(req.params.id)
+  res.send(req.params.id)
 })
 app.put('/test-list/:id', function(req, res) {
-    var data = null;
-    if (req.body && req.body.id) {
-        data = req.body;
-        res.send(data);
-    } else {
-        var body = '';
-        req.on('data', function(chunk) {
-            body += chunk; //读取参数流转化为字符串
-        });
-        req.on('end', function() {
-            try {
-                data = JSON.parse(body);
-            } catch (err) {
-                data = body;
-            }
-            res.send(data);
-        })
-    }
+  var data = null;
+  if (req.body && req.body.id) {
+    data = req.body;
+    res.send(data);
+  } else {
+    var body = '';
+    req.on('data', function(chunk) {
+      body += chunk; //读取参数流转化为字符串
+    });
+    req.on('end', function() {
+      try {
+        data = JSON.parse(body);
+      } catch (err) {
+        data = body;
+      }
+      res.send(data);
+    })
+  }
 })
 app.post('/test-list/:id', function(req, res) {
-    var data = null;
-    if (req.body && req.body.id) {
-        data = req.body;
-        res.send(data);
-    } else {
-        var body = '';
-        req.on('data', function(chunk) {
-            body += chunk; //读取参数流转化为字符串
-        });
-        req.on('end', function() {
-            try {
-                data = JSON.parse(body);
-            } catch (err) {
-                data = body;
-            }
-            res.send(data);
-        })
-    }
+  var data = null;
+  if (req.body && req.body.id) {
+    data = req.body;
+    res.send(data);
+  } else {
+    var body = '';
+    req.on('data', function(chunk) {
+      body += chunk; //读取参数流转化为字符串
+    });
+    req.on('end', function() {
+      try {
+        data = JSON.parse(body);
+      } catch (err) {
+        data = body;
+      }
+      res.send(data);
+    })
+  }
 })
 
 var server = app.listen(4000, function() {
-    var host = server.address().address;
-    var port = server.address().port;
-    console.log('Example app listening at http://%s:%s', host, port);
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Example app listening at http://%s:%s', host, port);
 });
